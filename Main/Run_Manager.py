044a8e115d361bdcf95abf123e0f183f786302a7
Chengwei Xia <cx220@ae-gr1610-02.ae.ic.ac.uk>
2024-04-23 21:30:08 +0100

    Add logging function

--- Main/Run_Manager.py
+++ Main/Run_Manager.py
@@ -63,19 +63,20 @@ def constraints_callback(u: cp.Variable, y: cp.Variable) -> List[Constraint]:
 ###### Setup parameters ###### 
 
 
-LAMBDA_G_REGULARIZER = np.array([0.1])   # Regularization on g (see DeePC paper, eq. 8)
-LAMBDA_Y_REGULARIZER = np.array([1500])  # Regularization on sigmay (see DeePC paper, eq. 8)
-LAMBDA_U_REGULARIZER = np.array([5])   # Regularization on sigmau
+LAMBDA_G_REGULARIZER = np.array([1,10])   # Regularization on g (see DeePC paper, eq. 8)
+LAMBDA_Y_REGULARIZER = np.array([10])  # Regularization on sigmay (see DeePC paper, eq. 8)
+LAMBDA_U_REGULARIZER = np.array([1])   # Regularization on sigmau
 LAMBDA_PROJ_REGULARIZER = [1e-4,1e-3,1e-2]
 s = 1                       # How many steps to apply predicted control in receding horizon, usually apply only one step
-EXPERIMENT_HORIZON = 1200    # Total number of steps
+EXPERIMENT_HORIZON = 10   # Total number of steps
 R = 0                      # Weight for R matrix in optimization (uT*R*u)
 Q = 1e5                    # Weight for Q matrix in optimization (yT*Q*y)
 umin = -0.1
 umax = 0.1
-yref = 0
+yref = -0.62
+solver = cp.SCS
 
-T_INI = 150                # Size of the initial set of data
+T_INI = 150               # Size of the initial set of data
 T_tr = 1000
 T_list = [T_tr]              # Number of data points used to estimate the system
 HORIZON = 150             # Horizon length
@@ -83,11 +84,11 @@ dim_u = 1 # The number of control actions, e.g. only 1 mass flow rate is needed 
 dim_y = 1 # The number of measurements, e.g. 64 sensors for pressure or 32 for antisymmetric pressure measurements
 Hankel_up_steps = 50 # The number of steps after which we update online Hankel matrices
 
-folder_data = "Offline_Data_" + str(T_tr)
+folder_data = "Offline_Data_" + str(T_tr) + "_drag"
 
 cano_qp = True
 Use_offline_data = True
-Online_hankel = True
+Online_hankel = False
 
 
 def DeePC_Run(data_path, opt_params, hankel_params, selectors):
@@ -107,6 +108,7 @@ def DeePC_Run(data_path, opt_params, hankel_params, selectors):
     umin     = opt_params['umin']
     umax     = opt_params['umax']
     yref     = opt_params['yref']
+    solver   = opt_params['solver']
     
     
     ##### Hankel Matrix Variables #####
@@ -124,7 +126,8 @@ def DeePC_Run(data_path, opt_params, hankel_params, selectors):
     Online_hankel = selectors['Online_hankel']
     
     
-    sum_reward = 0
+    mse = 0
+    total_time = 0
     
     sys = FlowSystem()
 ##### Data Collection (Parallelization needs development) ######
@@ -203,12 +206,13 @@ def DeePC_Run(data_path, opt_params, hankel_params, selectors):
         start_time = time.perf_counter()
         ## Run DeePC control to obtain the first set of actions
         if cano_qp:
-            Uo, Yo = deepc.qp_solve(data_ini = data_ini, warm_start=True)
+            Uo, Yo = deepc.qp_solve(data_ini = data_ini, warm_start=True, solver = solver)
         elif not cano_qp:
-            Uo, info = deepc.solve(data_ini = data_ini, warm_start=True)
+            Uo, info = deepc.solve(data_ini = data_ini, warm_start=True , solver = solver)
         end_time = time.perf_counter()
         print('One optimization step takes :% s' % ((end_time - start_time)))
         
+        total_time += end_time - start_time
         # Apply optimal control input for one step
         _ = sys.apply_input(u = Uo[:s, :], noise_std=0)
         
@@ -228,14 +232,45 @@ def DeePC_Run(data_path, opt_params, hankel_params, selectors):
         print("One action step applied. On action step ", k+1, "out of ", EXPERIMENT_HORIZON//s, ".")
         
     data = sys.get_all_samples()
+    
+    mse = np.mean((data.y[-20:]-yref)**2) #mse for last 20 steps
+    
+    info = {'T':T_tr,
+            'Tini':T_INI,
+            'Tf':HORIZON,
+            'lambda_g': LAMBDA_G_REGULARIZER,
+            'lambda_y': LAMBDA_Y_REGULARIZER,
+            'lambda_u': LAMBDA_U_REGULARIZER,
+            'R': R,
+            'Q': Q,
+            'solver':solver,
+            'avg_opt_step_time':total_time/(EXPERIMENT_HORIZON//s),
+            'total_steps':EXPERIMENT_HORIZON,
+            'mse':mse
+            }
+
         
-    return data #, sum_reward
-        
-    #sys.close()
-    print("Finish DeePC.")
+    return data, info #, sum_reward
 
+def log_params(info):
+    
+    print("Start parameter logging.")
+    name = "log_params.csv"
+    if (not os.path.exists("logging")):
+        os.mkdir("logging")
+    if (not os.path.exists("logging/" + name)):
+        with open("logging/" + name, "w") as csv_obj:
+            spam_writer = csv.writer(csv_obj, delimiter=";", lineterminator="\n")
+            spam_writer.writerow(["T" ,"Tini" ,"Tf" ,"lambda_u" ,"lambda_y" ,"lambda_g" ,"Q" ,"R" ,"solver" ,"avg_opt_step_time", "total_steps", "mse"])
+            spam_writer.writerow([info['T'], info['Tini'], info['Tf'],info['lambda_u'],info['lambda_y'],info['lambda_g'],info['Q'],info['R'],info['solver'],info['avg_opt_step_time'],info['total_steps'],info['mse']])
+    else:
+        with open("logging/" + name, "a") as csv_state:
+            spam_writer = csv.writer(csv_state, delimiter=";", lineterminator="\n")
+            spam_writer.writerow([info['T'], info['Tini'], info['Tf'],info['lambda_u'],info['lambda_y'],info['lambda_g'],info['Q'],info['R'],info['solver'],info['avg_opt_step_time'],info['total_steps'],info['mse']])
+    print("Finish parameter logging.")
+    
 
-
+#### Main code for running ####
 data_path = {'offline_data': folder_data}
 
 
@@ -268,11 +303,15 @@ for i in range(len(LAMBDA_G_REGULARIZER)):
                           'Q': Q,
                           'umin':umin,
                           'umax':umax,
-                          'yref':yref
+                          'yref':yref,
+                          'solver':solver
                           }
             
             # Run control experiments
-            data = DeePC_Run(data_path=data_path, opt_params=opt_params, hankel_params=hankel_params, selectors=selectors)
+            data,info = DeePC_Run(data_path=data_path, opt_params=opt_params, hankel_params=hankel_params, selectors=selectors)
+            
+            # Save parameter logs
+            log_params(info)
             
             # Plot curve
             fig, ax = plt.subplots(1,2)
@@ -281,7 +320,7 @@ for i in range(len(LAMBDA_G_REGULARIZER)):
             ax[0].plot(data.y[T_INI:], label=f'$s={s}, T={T_tr}, T_i={T_INI}, T_f={HORIZON}$')
             ax[1].plot(data.u[T_INI:], label=f'$s={s}, T={T_tr}, T_i={T_INI}, T_f={HORIZON}$')
 
-            ax[0].set_ylim(-0.2, 0.2)
+            ax[0].set_ylim(-0.6, -0.8)
             ax[1].set_ylim(-0.1, 0.1)
             ax[0].set_xlabel('t')
             ax[0].set_ylabel('y')
@@ -292,11 +331,13 @@ for i in range(len(LAMBDA_G_REGULARIZER)):
             ax[0].set_title('Closed loop - output signal $y_t$')
             ax[1].set_title('Closed loop - control signal $u_t$')
             plt.legend(fancybox=True, shadow=True)
-            fig_name = f'T_{T_tr}_Tini_{T_INI}_Tf_{HORIZON}_g_' + "{:.1f}".format(LAMBDA_G_REGULARIZER[i]).replace('.','p') + '_y_' + "{:.1f}".format(LAMBDA_Y_REGULARIZER[j]).replace('.','p') + '_u_' + "{:.1f}".format(LAMBDA_U_REGULARIZER[k]).replace('.','p')
+            fig_name = f'drag_T_{T_tr}_Tini_{T_INI}_Tf_{HORIZON}_g_' + "{:.1f}".format(LAMBDA_G_REGULARIZER[i]).replace('.','p') + '_y_' + "{:.1f}".format(LAMBDA_Y_REGULARIZER[j]).replace('.','p') + '_u_' + "{:.1f}".format(LAMBDA_U_REGULARIZER[k]).replace('.','p')
             plt.savefig(fname = fig_name)
             plt.show()
             
             y_mse = np.mean(data.y[-10:,:]**2)
 
+    #sys.close()
+print("Finish DeePC.")
 #if test_hankel_params == True:
     #for i in range(len(LAMBDA_G_REGULARIZER)):
